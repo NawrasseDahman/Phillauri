3019bea880543dba6f363a874433be7c746a1af8
Nawrasse Dahman <nawrassehimself@pm.me>
2021-09-17 23:29:15 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,71 @@
+require('dotenv')
+
+const { Client,Util, Collection,MessageEmbed,Structures } = require("discord.js");
+
+const keepAlive = require('./server.js')
+
+keepAlive()
+
+async function errorEmbed(text,message){
+
+      const newembed = new Discord.MessageEmbed()
+
+      .setColor("#FF7676");
+
+       return message.channel.send(newembed);
+
+    }
+
+const Discord = require('discord.js');
+
+const client = new Client({
+
+    disableEveryone: true
+
+})
+
+const axios = require("axios")
+
+const channel_id = "888159441638260740"
+
+client.on("ready", () => {
+
+  console.log(`The bot has started`);
+
+  client.user.setActivity(`Your Wishes | Say Hi`, { type: 'LISTENING'});
+
+});
+
+client.on('message', async (message) => {
+
+  if (!message.guild) return;
+
+  if (message.author.bot) return;
+
+  try {
+
+  if (message.channel.id != channel_id)
+
+   return
+
+  let res = await axios.get(`http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=1&msg=${encodeURIComponent(message.content)}`);
+
+  message.reply(res.data.cnt);
+
+  } catch {
+
+   errorEmbed(`Bot error, please try again!`,message)
+
+   }
+
+})
+
+client.on('ready', async () => {
+
+    console.clear()
+
+    console.log(`${client.user.tag} is online!`)
+
+})
+
+client.login(process.env.TOKEN);
